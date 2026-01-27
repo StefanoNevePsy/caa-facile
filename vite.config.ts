46f448f5a66fb7df9560e4515fc68d6c8754ab7d
Stefano Neve <nevestefanopsy@gmail.com>
2026-01-27 10:20:05 +0100

    fix model path mac

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // <--- FONDAMENTALE per Electron
+  base: './',
+  resolve: {
+    alias: {
+      '@xenova/transformers': '@xenova/transformers/dist/transformers.js',
+    },
+  },
 })
